18ec198fae0f6932f54244445bc36c6145021cec
kristinakaleo <kaleosalie@gmail.com>
2023-09-23 02:03:55 +0700

    Initial Commit

---
+++ HelloHelloWorld1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloHelloWorld1
+//
+//  Created by Кристина on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
